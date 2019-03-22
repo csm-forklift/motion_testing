9d1018b026972da450470387a08b93b28e07583a
larsenkw <kylew.larsen@gmail.com>
2019-03-22 10:52:29 -0600

    Initial commit

---
+++ src/accelerator_node.cpp
@@ -0,0 +1,90 @@
+/**
+ * 'accelerator_node' is intended to read directly from the joystick node and
+ * get the analog values from the left joystick. The "Up/Down" axis is the only
+ * value of interest and directly controls the signal sent to the accelerator
+ * pedal through the Arduino. The accelerator can only control the velocity in
+ * a single direction (based on which gear the forklift is in, e.g. forward or
+ * reverse).
+ *
+ * You should use the "X" mode for the controller as this code will be designed
+ * for that button/axes configuration.
+ */
+
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+#include <sensor_msgs/Joy.h>
+
+class AcceleratorNode
+{
+private:
+    // ROS Objects
+    ros::NodeHandle nh; // global handle
+    ros::NodeHandle nh_; // local handle
+    ros::Subscriber joy_sub; // subscribes to raw joystick message
+    ros::Publisher throttle_switch_pub; // publishes on/off signal for throttle
+    ros::Publisher pedal_fraction_pub; // publishes fraction pedal is pressed
+
+    // Parameters
+    int deadman_button; // assigns which button to check in order to send motion commands
+    double scale_linear; // scales accelerator command by this factor before sending
+
+public:
+    AcceleratorNode() : nh(""), nh_("~")
+    {
+        // Set up ROS objects
+        joy_sub = nh.subscribe<sensor_msgs::Joy>("joy", 1, &AcceleratorNode::joyCallback, this);
+        throttle_switch_pub = nh_.advertise<std_msgs::Bool>("throttle_switch", 1);
+        pedal_fraction_pub = nh_.advertise<std_msgs::Float32>("pedal_fraction", 1);
+
+        // Initialize Parameters
+        nh_.param<int>("deadman", deadman_button, 4);
+        nh_.param<double>("scale_linear", scale_linear, 1.0);
+    }
+
+    void joyCallback(const sensor_msgs::Joy::ConstPtr& msg)
+    {
+        // Reads the joystick deadman button, which controls the relay for turning on the pedal control, and the left joystick "Up/Down" axis, which determines the pedal fraction to send to the Arduino to control the velocity.
+
+        // Message data
+        std_msgs::Bool throttle_switch;
+        std_msgs::Float32 pedal_fraction;
+
+        // Check deadman button
+        // If it is pressed, it should cause the throttle switch relay to turn on allowing the forklift to receive velocity commands.
+        if (msg->buttons[deadman_button] == 0) {
+            throttle_switch.data = false;
+            throttle_switch_pub.publish(throttle_switch);
+
+            // Send a 0 velocity signal to be sure there is no motion
+            pedal_fraction.data = 0.0;
+            pedal_fraction_pub.publish(std_msgs::Float32(pedal_fraction));
+        }
+        else {
+            throttle_switch.data = true;
+            throttle_switch_pub.publish(throttle_switch);
+
+            // Grab the left analog "Up/Down" value and scale before sending
+            pedal_fraction.data = msg->axes[1];
+
+            // Bound between 0 and 1
+            pedal_fraction.data = std::max(0.0, static_cast<double>(pedal_fraction.data));
+            pedal_fraction.data = std::min(1.0, static_cast<double>(pedal_fraction.data));
+
+            // Scale
+            pedal_fraction.data *= scale_linear;
+            pedal_fraction_pub.publish(pedal_fraction);
+        }
+    }
+};
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "accelerator_node");
+
+    AcceleratorNode accelerator_node;
+
+    ros::spin();
+    return 0;
+}
---
+++ src/accelerator_write/accelerator_write.ino
@@ -0,0 +1,83 @@
+/**
+ * This code allows you to write a signal to a relay to act as
+ * the switch for the accelerator and to then send two signals
+ * that mimic the potentiometers to control the forklift speed.
+ * 
+ * This code is intended to be used as with an Arduino as a 
+ * serial node in ROS and receives the "throttle_switch" and 
+ * "pedal_fraction" topic messages from the "accelerator_node"
+ * node.
+ */
+ 
+// ROS Includes
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+
+// Function declarations
+void switchCallback(const std_msgs::Bool&);
+void pedalCallback(const std_msgs::Float32&);
+
+//===== Pins =====//
+const int RELAY_PIN = 2;
+const int SIGNAL_PIN_1 = 3;
+const int SIGNAL_PIN_2 = 4;
+const int DEBUG_LED = 13;
+
+//===== Data Variables =====//
+// You want to "MIN" and "MAX" pwm values to produce voltages 
+// that match the min and max of the signals from the forklift
+const int PWM_MIN = 15;  
+const int PWM_MAX = 200; 
+bool g_throttle_switch;
+float g_pedal_fraction;
+
+//===== ROS Objects =====//
+ros::NodeHandle nh;
+ros::Subscriber<std_msgs::Bool> throttle_switch_sub("accelerator_node/throttle_switch", &switchCallback);
+ros::Subscriber<std_msgs::Float32> pedal_fraction_sub("accelerator_node/pedal_fraction", &pedalCallback);
+
+void setup() {
+  // Set up ROS
+  nh.initNode();
+  nh.subscribe(throttle_switch_sub);
+  nh.subscribe(pedal_fraction_sub);
+    
+  // Set up pin modes
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(SIGNAL_PIN_1, OUTPUT);
+  pinMode(SIGNAL_PIN_2, OUTPUT);
+  pinMode(DEBUG_LED, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
+  analogWrite(SIGNAL_PIN_1, PWM_MIN);
+  analogWrite(SIGNAL_PIN_2, PWM_MIN);
+}
+
+void loop() {
+  // Update switch relay
+  if (g_throttle_switch) {
+    digitalWrite(RELAY_PIN, LOW);
+  }
+  else {
+    digitalWrite(RELAY_PIN, HIGH);
+  }
+
+  // Update pedal signals
+  int pwm_signal = map(100*g_pedal_fraction, 0, 100, PWM_MIN, PWM_MAX);
+  analogWrite(SIGNAL_PIN_1, pwm_signal);
+  analogWrite(SIGNAL_PIN_2, pwm_signal);
+
+  nh.spinOnce();
+  delay(1);
+}
+
+void switchCallback(const std_msgs::Bool& msg)
+{
+  g_throttle_switch = msg.data;
+}
+
+void pedalCallback(const std_msgs::Float32& msg)
+{
+  g_pedal_fraction = msg.data;
+}
+
